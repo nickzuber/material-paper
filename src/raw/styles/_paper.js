10de6beddb564a84445d8c3dd91ef66235d1bbee
nickzuber <nickzoober@gmail.com>
2016-04-16 01:14:52 -0400

    minor adjustments

--- src/raw/styles/_paper.js
+++ src/raw/styles/_paper.js
@@ -16,7 +16,8 @@ exports = module.exports = {
     margin: 0,
     fontSize: '16px',
     borderRadius: '2px',
-    boxShadow: '0 1px 6px rgba(0, 0, 0, 0.12), 0 1px 4px rgba(0, 0, 0, 0.24)'
+    boxShadow: '0 1px 6px rgba(0, 0, 0, 0.12), 0 1px 4px rgba(0, 0, 0, 0.24)',
+    transition: 'all 500ms cubic-bezier(0.23, 1, 0.32, 1) 0s'
   },
 
   midUpperLevel: {
